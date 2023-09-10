1afb3ac4a05ff03fdef6f92696a3d79cea1c6e62
MiFy <81895134+XDMiFy@users.noreply.github.com>
2023-09-10 13:05:04 +0600

    Create first_lesson.kt

---
+++ first_lesson.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var x : Int = 5 
+    x = x + 4 
+    x = -7
+    println(x) 
+     
+    val y : Int = 6 
+     
+   x = x + y 
+   println(x) 
+   
+   var name : String = "Nikolai" 
+   val desc : String = "- лапочка" 
+   
+   name = name + desc 
+   println(name) 
+}
