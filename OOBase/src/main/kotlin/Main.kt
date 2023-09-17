0f37c0d448a65f9809af39839a916d905e9ca2c6
MiFy <81895134+XDMiFy@users.noreply.github.com>
2023-09-17 12:54:40 +0600

    if task

---
+++ OOBase/src/main/kotlin/Main.kt
@@ -0,0 +1,69 @@
+fun main(args: Array<String>) {
+
+
+
+}
+// задачи по теме if-else
+fun  ifFirsttask(){
+    var x = 6
+    var y = 8
+
+    if (y != 0) {
+        println(x /y)
+    }else{
+        println("Делить на ноль нельзя")
+    }
+
+    println("Программа закончена")
+}
+
+fun  ifSecondtask(){
+    var p = 4
+    var b = 2
+    var fg = p + b
+    if (fg > 0) {
+        println("Больше нуля")
+    }else{
+        println("Меньше нуля")
+    }
+
+}
+
+fun ifThirdTask(){
+    val month = 12
+    if (month == 12){
+        println("декабрь")
+    }
+    else if (month == 11){
+        println("ноябрь")
+    }
+    else if (month == 10){
+        println("октябрь")
+    }
+    else if (month == 9){
+        println("сентябрь")
+    }
+    else if (month == 8){
+        println("август")
+    }
+    else if (month == 7){
+        println("июль")
+    }
+    else if (month == 6){
+        println("июнь")
+    }
+    else if (month == 5){
+        println("май")
+    }
+    else if (month == 4){
+        println("апрель")
+    }else if (month == 3){
+        println("март")
+    }else if (month == 2){
+        println("февраль")
+    }else if (month == 1){
+        println("январь")
+    }else{
+
+    }
+}
